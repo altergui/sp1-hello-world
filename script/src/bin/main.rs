f631a2afbf2914ea3eb498d2e0d0e9cf6a190cf1
Gui Iribarren <gui@altermundi.net>
2024-09-18 09:43:29 +0200

    generate groth16 proof

--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -85,6 +85,7 @@ fn main() {
         // Generate the proof
         let proof = client
             .prove(&pk, stdin)
+            .groth16()
             .run()
             .expect("failed to generate proof");
 
