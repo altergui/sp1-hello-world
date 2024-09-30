dabdac412096be38eece42f6663b4596aebc3379
Gui Iribarren <gui@altermundi.net>
2024-09-30 12:30:12 +0200

    playing with monotree

--- program/src/main.rs
+++ program/src/main.rs
@@ -10,6 +10,10 @@ sp1_zkvm::entrypoint!(main);
 
 use alloy_sol_types::SolType;
 use fibonacci_lib::{fibonacci, PublicValuesStruct};
+use monotree::database::*;
+use monotree::hasher::*;
+use monotree::utils::*;
+use monotree::*;
 
 pub fn main() {
     // Read an input to the program.
@@ -25,6 +29,35 @@ pub fn main() {
 
     // offset for fun
     let (a, b) = (a + offset, b + offset);
+
+    let current_id = b.to_string(); // Get current fibonacci number as a String
+
+    // Combine all process IDs
+    let mut process_ids = vec![current_id];
+
+    // Init a monotree instance:
+    // manually select a db and a hasher as your preference
+    // Monotree::<DATABASE, HASHER>::new(DB_PATH)
+    // where DATABASE = {MemoryDB, RocksDB, Sled}
+    //         HASHER = {Blake3, Blake2s, Blake2b, Sha2, Sha3}
+    let mut tree = Monotree::<MemoryDB, Blake3>::new("/tmp/monotree");
+
+    // It is natural the tree root initially has 'None'
+    let root = None;
+
+    // Prepare a random pair of key and leaf.
+    // random_hash() gives a fixed length of random array,
+    // where Hash -> [u8; HASH_LEN], HASH_LEN = 32
+    let key = random_hash();
+    let leaf = random_hash();
+
+    // Insert the entry (key, leaf) into tree, yielding a new root of tree
+    let root = tree.insert(root.as_ref(), &key, &leaf);
+    assert_ne!(root, None);
+
+    // Get the leaf inserted just before. Note that the last root was used.
+    let found = tree.get(root.as_ref(), &key);
+    assert_eq!(found, Some(leaf));
 
     // Encode the public values of the program.
     let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { n, a, b });
